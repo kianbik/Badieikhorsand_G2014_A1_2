aebda154b310c04d3dfc925312e1c0e994b6122e
Kian <kianbadiei@gmail.com>
2021-10-04 03:51:12 -0400

    All Scenes Completed

---
+++ Assets/UI Scripts/MainMenuButton.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class MainMenuButton : MonoBehaviour
+{
+    public void OnMenuButtonPressed()
+    {
+        SceneManager.LoadScene("Start");
+    }
+}
